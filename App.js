9bbf4ad0e2464308943435e0284cc77c800d9500
wanderson_veiga <wanderalez@gmail.com>
2019-08-24 20:31:43 -0300

    fix missing import

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './src/store/configureStore';
 import { useScreens } from 'react-native-screens';
