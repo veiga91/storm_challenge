a63dfc38042ad886656f72b83d9149074035296d
wanderson_veiga <wanderalez@gmail.com>
2019-08-24 14:09:22 -0300

    link eact-native-screens

--- android/app/src/main/java/com/marvel/MainActivity.java
+++ android/app/src/main/java/com/marvel/MainActivity.java
@@ -1,8 +1,13 @@
 package com.marvel;
 
-import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import com.facebook.react.ReactFragmentActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactFragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(null);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
