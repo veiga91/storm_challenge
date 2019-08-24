389e30af236fef377d451b934021d764c53cac5d
wanderson_veiga <wanderalez@gmail.com>
2019-08-24 20:34:08 -0300

    add home selectors

---
+++ src/store/selectors/home.js
@@ -0,0 +1,3 @@
+export const getLoadingState = (state) => state.home.fetching;
+export const getCharactersData = (state) => state.home.characters;
+export const getFetchingError = (state) => state.home.error;
