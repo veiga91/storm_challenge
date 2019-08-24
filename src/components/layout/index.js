e1516df3c5873fd2770f03d0a6e62943df6f171c
wanderson_veiga <wanderalez@gmail.com>
2019-08-24 20:32:45 -0300

    add containers

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,20 @@
 import styled from 'styled-components/native';
+import { scale } from 'react-native-size-matters';
 
 export const Container = styled.View`
   height: 100%;
-  background-color: red;
+  background-color: transparent;
+`;
+
+export const LoadingContainer = styled(Container)`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ErrorContainer = styled(Container)`
+  justify-content: center;
+  align-items: center;
+  padding-right: ${scale(20)};
+  padding-left: ${scale(20)};
 `;
 
