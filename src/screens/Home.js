08b28e6d1b64b8248da54c9815139a60202ba210
wanderson_veiga <wanderalez@gmail.com>
2019-08-24 20:34:28 -0300

    update home component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,64 @@
 import React, { PureComponent } from 'react';
-import { Container } from '../components/layout';
+import { ActivityIndicator, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchCharacters } from '../store/actions/home';
+import { Container, LoadingContainer, ErrorContainer } from '../components/layout';
+import { getCharactersData, getFetchingError, getLoadingState } from '../store/selectors/home';
 
 class Home extends PureComponent {
-  render() {
+  componentDidMount() {
+    this.props.fetchCharacters();
+  }
+
+  handleLoadingState = () => {
+    const { isLoading, error } = this.props;
+
+    if (isLoading) {
+      return (
+        <LoadingContainer>
+          <ActivityIndicator />
+        </LoadingContainer>
+      );
+    }
+    
+    if (error) {
+     return (
+       <ErrorContainer>
+         <Text>{error}</Text>
+       </ErrorContainer>
+     );
+    }
+    
+    if (this.props.characters.length === 0) {
+      return (
+        <ErrorContainer>
+          <Text>Lista vazia</Text>
+        </ErrorContainer>
+      );
+    }
+
     return (
       <Container>
-
+        <Text>{this.props.characters[0].id}</Text>
       </Container>
     );
   }
+
+  render() {
+    return this.handleLoadingState();
+  }
 }
 
-export default Home;
+const mapStateFormProps = (state, ownProps) => {
+  return {
+    ...ownProps,
+    isLoading: getLoadingState(state),
+    characters: getCharactersData(state),
+    error: getFetchingError(state)
+  }
+};
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({ fetchCharacters }, dispatch);
+
+export default connect(mapStateFormProps, mapDispatchToProps)(Home);
