f1cbe92988d01cd8da2f40b5f39cf95870da5369
wanderson_veiga <wanderalez@gmail.com>
2019-08-24 14:39:41 -0300

    create home screen

---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import { Container } from '../components/layout';
+
+class Home extends PureComponent {
+  render() {
+    return (
+      <Container>
+
+      </Container>
+    );
+  }
+}
+
+export default Home;
