61445d1d6259244452421d987dc4f919056eb44f
wanderson_veiga <wanderalez@gmail.com>
2019-08-24 20:33:11 -0300

    update home service

--- src/services/home.js
+++ src/services/home.js
@@ -1,8 +1,9 @@
 import axiosInstance from './configureService';
+import { API_KEY, HASH, TIME_STAMP } from 'react-native-dotenv';
 
 export const getCharacters = async () => {
   try {
-    const payload = await axiosInstance();
+    const payload = await axiosInstance(`characters?apikey=${API_KEY}&hash=${HASH}&ts=${TIME_STAMP}`);
     return payload.data.data.results;
   } catch (e) {
     throw(e);
