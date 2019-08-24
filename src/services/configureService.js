13b7793b6e15166711732e48c63b1c1d7e97bf86
wanderson_veiga <wanderalez@gmail.com>
2019-08-24 20:33:00 -0300

    update service instance

--- src/services/configureService.js
+++ src/services/configureService.js
@@ -1,13 +1,7 @@
 import axios from 'axios';
-import { API_KEY, HASH, TIME_STAMP } from 'react-native-dotenv';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://gateway.marvel.com/v1/public/characters',
-  params: {
-    apikey: API_KEY,
-    hash: HASH,
-    ts: TIME_STAMP
-  }
+  baseURL: 'http://gateway.marvel.com/v1/public',
 });
 
 export default axiosInstance;
