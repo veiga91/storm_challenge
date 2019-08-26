cf987d5394a63603344077b7e544952dd98edd36
wanderson_veiga <wanderalez@gmail.com>
2019-08-26 08:19:58 -0300

    add detail to routes

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -1,21 +1,27 @@
 import { createAppContainer, createStackNavigator } from "react-navigation";
 import HomeScreen from '../screens/Home';
+import DetailScreen from '../screens/Detail';
 
 // Routes
 const Home = {
   screen: HomeScreen
 };
 
+const Detail = {
+  screen: DetailScreen
+}
+
 //Nnavigation config
 
 const rootRoutesConfig = {
   initialRouteName: 'Home',
-  headerMode: 'none',
+  //headerMode: 'none',
 };
 
 // Navigator
 const Navigator = createStackNavigator({
-  Home
+  Home,
+  Detail
 }, rootRoutesConfig);
 
 export default createAppContainer(Navigator);
