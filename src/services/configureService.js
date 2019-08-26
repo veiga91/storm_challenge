70b6593bf1c04560dd35deda4a9a1c4396437d0f
wanderson_veiga <wanderalez@gmail.com>
2019-08-26 08:19:41 -0300

    improve axios instance

--- src/services/configureService.js
+++ src/services/configureService.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
+import { API_KEY, HASH, TIME_STAMP } from 'react-native-dotenv';
+
 
 const axiosInstance = axios.create({
-  baseURL: 'http://gateway.marvel.com/v1/public',
+  baseURL: 'http://gateway.marvel.com/v1/public/characters'
 });
+
+export const PARAMS = {
+  apikey: API_KEY,
+  hash: HASH,
+  ts: TIME_STAMP
+}
 
 export default axiosInstance;
