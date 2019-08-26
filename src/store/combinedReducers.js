51c829de98f47d6e93e02b898c7897d30e0b0a2b
wanderson_veiga <wanderalez@gmail.com>
2019-08-26 08:19:21 -0300

    add detail reducer

--- src/store/combinedReducers.js
+++ src/store/combinedReducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import home from './reducers/home';
+import detail from './reducers/detail';
 
 export default combineReducers({
-  home
+  home,
+  detail
 });
