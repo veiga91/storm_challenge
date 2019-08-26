dfb14fe273d0cf38c8fa067c31102f5374123bf3
wanderson_veiga <wanderalez@gmail.com>
2019-08-26 08:17:59 -0300

    update home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,27 +1,57 @@
 import React, { PureComponent } from 'react';
-import { ActivityIndicator, Text } from 'react-native';
+import { Text, Image, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchCharacters } from '../store/actions/home';
-import { Container, LoadingContainer, ErrorContainer } from '../components/layout';
+import { Container, ErrorContainer } from '../components/layout';
 import { getCharactersData, getFetchingError, getLoadingState } from '../store/selectors/home';
+import List from '../components/list/List';
+import CharacterThumb from '../components/list/CharacterItem';
+import { withTheme } from 'styled-components';
+import logo from '../assets/logo.jpg';
+import magnify from '../assets/magnify.png';
 
 class Home extends PureComponent {
+
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: '#000'
+    },
+    headerLeft: <Image style={{ width: 100, height: 40, marginLeft: 20 }} source={logo} />,
+    headerRight: (
+      <TouchableOpacity style={{ width: 50, height: 40 }} onPress={() => {}}>
+        <Image source={magnify} style={{ width: 50, height: 40 }}/>
+      </TouchableOpacity>
+    ),
+  }
+
   componentDidMount() {
     this.props.fetchCharacters();
   }
 
-  handleLoadingState = () => {
-    const { isLoading, error } = this.props;
-
-    if (isLoading) {
-      return (
-        <LoadingContainer>
-          <ActivityIndicator />
-        </LoadingContainer>
-      );
-    }
+  _renderItem = ({ item }) => {
+    const uri = `${item.thumbnail.path}.${item.thumbnail.extension}`;
     
+    return (
+      <CharacterThumb
+        onPress={() => this.props.navigation.navigate('Detail', { id: item.id, uri })}
+        name={item.name}
+        loading={!item.loaded}
+        size="full"
+        uri={uri}
+      />
+    );
+  }
+
+  _getItemLayout = (_, index) => {
+    const { height } = this.props.theme.thumbs.full;
+
+    return { length: height, offset: height * index, index }
+  }
+
+  handleLoadingState = () => {
+    const { isLoading, error, characters } = this.props;
+
     if (error) {
      return (
        <ErrorContainer>
@@ -30,17 +60,15 @@ class Home extends PureComponent {
      );
     }
     
-    if (this.props.characters.length === 0) {
-      return (
-        <ErrorContainer>
-          <Text>Lista vazia</Text>
-        </ErrorContainer>
-      );
-    }
-
     return (
-      <Container>
-        <Text>{this.props.characters[0].id}</Text>
+      <Container bgColor="black">
+        <List
+          loading={isLoading}
+          data={characters}
+          extraData={isLoading}
+          renderItem={this._renderItem}
+          getItemLayout={this._getItemLayout}
+        />
       </Container>
     );
   }
@@ -61,4 +89,4 @@ const mapStateFormProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({ fetchCharacters }, dispatch);
 
-export default connect(mapStateFormProps, mapDispatchToProps)(Home);
+export default connect(mapStateFormProps, mapDispatchToProps)(withTheme(Home));
